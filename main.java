2edf9d6e0cd20813e837def14f96d3a849e172a3
Matthew Mar <158439983+Matthew-Mar-GH@users.noreply.github.com>
2024-09-05 22:07:05 -0700

    test

---
+++ main.java
@@ -0,0 +1,22 @@
+public class main {
+  public static void main(String[] args) {
+
+  }
+}
+sdfsd
+class Tank {
+  int health;
+  int defense;
+  int dmgmodifier;
+  int id;
+  public Tank(int health, int defense, int dmgmodifier, int id) {
+    this.health = health;
+    this.defense = defense;
+    this.dmgmodifier = dmgmodifier;
+    this.id = id;
+  }
+
+  public static void main(String[] args) {
+    Tank tank1 = new Tank();
+  }
+}
